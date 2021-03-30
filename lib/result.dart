3195c554514271eca08ec5713159773ae438e809
Akhil <avalsamrithan@rbbn.com>
2021-03-30 13:32:12 +0530

    Fixed deprecated buttons in old vscode
    Fix: Replaced RaisedButton and FlatButton with ElevatedButton and
    TextButton respectively.

--- lib/result.dart
+++ lib/result.dart
@@ -35,10 +35,10 @@ class Result extends StatelessWidget {
             ),
             textAlign: TextAlign.center,
           ),
-          FlatButton(
+          TextButton(
             onPressed: resetQuiz,
             child: Text('Restart Quiz!'),
-            textColor: Colors.blue,
+            style: TextButton.styleFrom(primary: Colors.green),
           ),
         ],
       ),
